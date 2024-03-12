f9ab7e9407189a33b0f1847f1bc5d17a8bfcec25
Emily LeBlanc <eleblanc@protonmail.com>
2024-03-12 12:35:34 -0400

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1 class="text-red-600">Welcome to SvelteKit</h1>
+<h1 class="text-red-600">Welcome to LeBlanc's Prompts</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
