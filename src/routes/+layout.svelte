526d4bd56a528ea24e6a5ba84961f4657635cdcf
Emily LeBlanc <eleblanc@protonmail.com>
2024-03-13 13:19:21 -0400

    use inject function

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,10 @@
     import "../app.pcss";
     import "../lib/styles/highlight-js.css";   
     import "../lib/styles/app.css";   
-    import { inject } from '@vercel/analytics'
+    import { inject } from '@vercel/analytics';
+    import { dev } from '$app/environment';
+
+    inject({ mode: dev ? 'development' : 'production' });
 
 </script>
 
