0c442b5202a185800fd974166d3da24d9ff152c5
Emily LeBlanc <116583655+eleblanc-ai@users.noreply.github.com>
2024-09-20 15:28:45 -0400

    Update options.js

--- src/lib/components/pages/home/stats/prompting-areas/options.js
+++ src/lib/components/pages/home/stats/prompting-areas/options.js
@@ -1,9 +1,10 @@
 const tooltips = { 0: ["Coding:","Python, Svelte, Flask, Testing"],
                    1: ["Math:","Propositional Logic, Discrete Mathematics, Algebra"],
                    2: ["Science:","CS, Biology, Neurology, Environment"],
-                   3: ["Software Documentation:","Code Explanation and Commenting, Technical tutorials"],
+                   3: ["Documentation:","Code Explanation and Commenting, Technical tutorials"],
                    4: ["Research Methodology:","Problem Definition, Literature Surveys, Theorem Proving"],
-                   5: ["Data visualization:","LaTeX, D3, Apex Charts"]
+                   5: ["Data visualization:","LaTeX, D3, Apex Charts"], 
+                   6: ["Project Planning:","OKRs, Jira"], 
                  }
 
 const colors = ["#5ad45a", "#00b7c7", "#0d88e6","#1a53ff", "#4421af", "#7c1158", "#b30000"]
@@ -11,14 +12,14 @@ const colors = ["#5ad45a", "#00b7c7", "#0d88e6","#1a53ff", "#4421af", "#7c1158",
 
 export const options = {
 
-    labels: ['Coding', 'Math', 'Science', 'Software documentation', 'Research methodology', 'Data Visualization'],
+    labels: ['Coding', 'Math', 'Science', 'Software documentation', 'Research methodology', 'Data Visualization', 'Project Planning'],
     dataLabels: {
         enabled: false,
         enabledOnSeries: undefined,
       },
 
     series: [1,1,1,1,1,1],
-    colors: ["#5ad45a", "#00b7c7", "#0d88e6","#1a53ff", "#4421af", "#b30000"],
+    colors: ["#5ad45a", "#00b7c7", "#0d88e6","#1a53ff", "#4421af", "#7c1158", "#b30000"],
     chart: {
         height: 270,
         width: '100%',
@@ -51,7 +52,7 @@ export const options = {
                         showAlways:true,
                         label: 'prompts',
                         formatter: function (w) {
-                            return `~2k+`;
+                            return `~3k+`;
                           }
                     },
                     value: {
