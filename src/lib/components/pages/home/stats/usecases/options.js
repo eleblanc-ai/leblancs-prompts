ef47fc7f294f33b811ba98b03a7ad423727a0d64
Emily LeBlanc <116583655+eleblanc-ai@users.noreply.github.com>
2024-09-20 15:32:52 -0400

    Update options.js

--- src/lib/components/pages/home/stats/usecases/options.js
+++ src/lib/components/pages/home/stats/usecases/options.js
@@ -4,11 +4,12 @@ const tooltips = {0:["Open & Closed QA:","Providing answers to various question 
                   3:["Multi-turn:","Extended dialogue with maintained context","#1a53ff"],
                   4:["Rewriting:","Rephrasing text with maintained meaning","#0d88e6"],
                   5:["Extraction:","Creating structured data from unstructured text","#00b7c7"],
-                  6:["Classification:","Labeling text based on specified attributes","#5ad45a"]
+                  6:["Classification:","Labeling text based on specified attributes","#f4a261"],
+                  7:["Writing Feedback:","Personal writing feedback, student writing feedback","#5ad45a"],
                  }
 
 export const options = {
-  labels: ['Open & Closed QA', 'Summarization','Chain-of-thought','Multi-turn','Rewriting','Extraction','Classification'],
+  labels: ['Open & Closed QA', 'Summarization','Chain-of-thought','Multi-turn','Rewriting','Extraction','Classification', 'Writing Feedback'],
 
   dataLabels: {
     enabled: false,
@@ -16,8 +17,8 @@ export const options = {
       fontFamily: 'Inter, sans-serif'
     }
   },
-  series: [5, 4, 4, 3, 2, 1, 1],
-  colors: ["#b30000", "#7c1158", "#4421af", "#1a53ff", "#0d88e6", "#00b7c7", "#5ad45a", "#8be04e", "#ebdc78"],
+  series: [5, 4, 4, 3, 2, 1, 1,4],
+  colors: ["#b30000", "#7c1158", "#4421af", "#1a53ff", "#0d88e6", "#00b7c7", "#5ad45a", "#8be04e", "#ebdc78","#f4a261"],
   chart: {
     height: 270,
     width: '100%',
